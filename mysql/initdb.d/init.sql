b0e62efa368c5c5349c6ff222eba3c597a4f9c4e
ukohank517 <ukohank517@gmail.com>
2023-04-13 17:52:55 +0900

    redefine table

--- mysql/initdb.d/init.sql
+++ mysql/initdb.d/init.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS `room_member` (
 CREATE TABLE IF NOT EXISTS `image` (
   room_id       VARCHAR(100)  NOT NULL,
   prompt        TEXT          NOT NULL,
+  image_url     VARCHAR(100)  NOT NULL,
   created_at    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   updated_at    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   PRIMARY KEY (room_id)
