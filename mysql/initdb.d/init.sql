c0270b5074bde4abbdd8d30ce3a59435b43937aa
ukohank517 <ukohank517@gmail.com>
2023-04-13 10:12:51 +0900

    fix table defi

--- mysql/initdb.d/init.sql
+++ mysql/initdb.d/init.sql
@@ -10,12 +10,13 @@ CREATE TABLE IF NOT EXISTS `image` (
   room_id       VARCHAR(100)  NOT NULL,
   prompt        TEXT          NOT NULL,
   created_at    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  updated_at    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
   PRIMARY KEY (room_id)
 );
 
 CREATE TABLE IF NOT EXISTS `chat` (
   room_id       VARCHAR(100)  NOT NULL,
-  ip_address    VARCHAR(100)  NOT NULL,
+  user_id       INT           NOT NULL,
   message       TEXT          NOT NULL,
   created_at    DATETIME(6)   NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY (created_at)
